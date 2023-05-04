4b60d464eb57e901d3417f7c365cfb5676f59348
Claudio Ippoliti <10165317+claudioippoliti@users.noreply.github.com>
2023-05-04 18:57:01 +0000

    Update App.jsx and game.js

--- 02-Game/src/logic/game.js
+++ 02-Game/src/logic/game.js
@@ -39,3 +39,61 @@ export const checkEndGame = (newBoard) => {
   // en el tablero
   return newBoard.every((square) => square !== null);
 };
+
+export const findBestMove = (board) => {
+  function minimax(board, depth, isMaximizingPlayer) {
+    // determinar si la búsqueda alcanzó la profundidad máxima o si el juego ha terminado
+    let score = checkWinnerFrom(board);
+    if (score === 10) {
+      return score - depth;
+    }
+    if (score === -10) {
+      return score + depth;
+    }
+    if (checkEndGame(board) === false) {
+      return 0;
+    }
+
+    // si el jugador actual es el maximizador
+    if (isMaximizingPlayer) {
+      let bestScore = -Infinity;
+      for (let i = 0; i < 9; i++) {
+        if (board[i] === null) {
+          board[i] = "X";
+          let score = minimax(board, depth + 1, false);
+          board[i] = null;
+          bestScore = Math.max(score, bestScore);
+        }
+      }
+      return bestScore;
+    }
+    // si el jugador actual es el minimizador
+    else {
+      let bestScore = Infinity;
+      for (let i = 0; i < 9; i++) {
+        if (board[i] === null) {
+          board[i] = "O";
+          let score = minimax(board, depth + 1, true);
+          board[i] = null;
+          bestScore = Math.min(score, bestScore);
+        }
+      }
+      return bestScore;
+    }
+  }
+
+  let bestScore = -Infinity;
+  let bestMove = -1;
+  for (let i = 0; i < 9; i++) {
+    if (board[i] === null) {
+      board[i] = "X";
+      let score = minimax(board, 0, false);
+      board[i] = null;
+      if (score > bestScore) {
+        bestScore = score;
+        bestMove = i;
+      }
+    }
+  }
+  return bestMove;
+};
